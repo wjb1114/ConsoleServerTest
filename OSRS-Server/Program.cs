4647f98353e814f0c7a0b6743a803a3acbbcd3e8
William Breece <kbd1114@gmail.com>
2020-05-05 20:03:33 -0500

    Init

---
+++ OSRS-Server/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OSRS_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Game Server";
+
+            Server.Start(16, 26950);
+
+            Console.ReadKey();
+        }
+    }
+}
